2fddc284fa6eb24a784849362d400dbdc9aeb13b
in1dw3 <in1dw3@HZ352033.elorrieta.int>
2026-01-14 19:11:00 +0100

    Algunos abances con los acces y los controladores

---
+++ CINELORRIETA/src/utiles/Controladores.java
@@ -0,0 +1,76 @@
+package utiles;
+
+import java.util.Scanner;
+
+public class Controladores {
+	private Scanner sc = new Scanner(System.in);
+
+	public int pideNumero(String text) {
+		int ret = -1;
+		boolean error = true;
+		do {
+			try {
+				System.out.println(text);
+				ret = sc.nextInt();
+				sc.nextLine();
+				error = !(ret > -1);
+			} catch (Exception e) {
+				System.out.println("No es un número, por favor ingrese un número");
+				error = true;
+				sc.nextLine();
+			}
+		} while (error);
+		return ret;
+	}
+
+	public double pideNumeroDouble(String text) {
+		double ret = -1;
+		boolean error = true;
+		do {
+			try {
+				System.out.println(text);
+				System.out.println("Recuerde usar , no .");
+				ret = sc.nextDouble();
+				sc.nextLine();
+				error = !(ret > -1);
+			} catch (Exception e) {
+				System.out.println("No es un número, por favor ingrese un número");
+				error = true;
+				sc.nextLine();
+			}
+		} while (error);
+		return ret;
+	}
+
+	public boolean pideBooleano(String text) {
+		boolean ret = false;
+		boolean error = true;
+		do {
+			try {
+				System.out.println(text);
+				String input = sc.nextLine().trim().toLowerCase().charAt(0) + "";
+				error = false;
+				if (input.equalsIgnoreCase("s")) {
+					ret = true;
+				} else if (input.equalsIgnoreCase("n")) {
+					ret = false;
+				} else {
+					System.out.println("Error escribe s/n");
+				}
+			} catch (Exception e) {
+				System.out.println("Error al leer la entrada.");
+				error = true;
+				sc.nextLine();
+			}
+		} while (error);
+		return ret;
+	}
+
+	public String leerDeTeclado() {
+		String ret = null;
+		System.out.print("Dame un texto: ");
+		ret = sc.nextLine();
+		return ret;
+	}
+
+}
