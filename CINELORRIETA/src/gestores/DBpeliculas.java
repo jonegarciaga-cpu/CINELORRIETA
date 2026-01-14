8ad226b0e04883182347e673ca568748d886a3d3
in1dw3 <in1dw3@HZ352033.elorrieta.int>
2026-01-14 19:52:06 +0100

    actualizacion para el resto del equipo

--- CINELORRIETA/src/gestores/DBpeliculas.java
+++ CINELORRIETA/src/gestores/DBpeliculas.java
@@ -98,17 +98,4 @@ public class DBpeliculas {
 		return ret;
 	}
 
-	public static void main(String[] args) {
-		DBpeliculas dBAcces = new DBpeliculas();
-		ArrayList<Pelicula> peliculas = dBAcces.getAllPeliculas();
-
-		// Mostramos el resultado
-		if (null == peliculas) {
-			System.out.println("No hay Peliculas");
-		} else {
-			for (int i = 0; i < peliculas.size(); i++) {
-				System.out.println(peliculas.get(i));
-			}
-		}
-	}
 }
---
+++ CINELORRIETA/src/menu/Menu.java
@@ -0,0 +1,24 @@
+package menu;
+
+import java.util.ArrayList;
+
+import gestores.DBpeliculas;
+import pojos.Pelicula;
+
+public class Menu {
+
+	private void verPeliculas() {
+		DBpeliculas dBAcces = new DBpeliculas();
+		ArrayList<Pelicula> peliculas = dBAcces.getAllPeliculas();
+
+		// Mostramos el resultado
+		if (null == peliculas) {
+			System.out.println("No hay Peliculas");
+		} else {
+			for (int i = 0; i < peliculas.size(); i++) {
+				System.out.println(peliculas.get(i));
+			}
+		}
+	}
+
+}// FIN
